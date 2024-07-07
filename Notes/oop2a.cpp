b6ddb55bccbea6668e4e91adb6f29f058bf607ec
JuJams <164974545+JuJams@users.noreply.github.com>
2024-07-07 13:58:38 -0400

    Add files via upload

---
+++ Notes/oop2a.cpp
@@ -0,0 +1,47 @@
+/*
+  oop.cpp
+  CS2124
+ */
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Date {
+    friend ostream& operator<<(ostream& os, const Date& rhs);
+public:
+    Date(int m, int d, int y) : month(m), day(d), year(y) {}
+private:
+    int month, day, year;
+};
+
+class Person {
+    friend ostream& operator<<(ostream& os, const Person& rhs);
+public:
+    //Person(const string& name) : name(name) { }
+    //Person(const string& name) : name(name), bday() { }
+    Person(const string& name, int m, int d, int y)
+        : name(name), bday(m, d, y) { }
+private:    
+    string name;
+    Date bday;
+};
+
+int main() {
+    //Person john("John");
+    Person john("John", 2, 5, 2024);
+    cout << john << endl;
+}
+
+ostream& operator<<(ostream& os, const Person& rhs) {
+    //os << "Person: " << rhs.name; //  << ", dob: " << rhs.bday;
+    os << "Person: " << rhs.name << ", dob: " << rhs.bday;
+    return os;
+}
+
+ostream& operator<<(ostream& os, const Date& rhs) {
+    os << rhs.month << '/' << rhs.day << '/' << rhs.year;
+    return os;
+}
+
---
+++ Notes/oop2c.cpp
@@ -0,0 +1,67 @@
+/*
+  oop2.cpp
+  CS2124
+ */
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+// class Date {
+//     friend ostream& operator<<(ostream& os, const Date& rhs);
+// public:
+//     Date(int m, int d, int y) : month(m), day(d), year(y) {}
+// private:
+//     int month, day, year;
+// };
+
+class Person {
+    friend ostream& operator<<(ostream& os, const Person& rhs);
+    //
+    class Date {
+        friend ostream& operator<<(ostream& os, const Date& rhs);
+        // friend ostream& operator<<(ostream& os, const Date& rhs) {
+        //     os << rhs.month << '/' << rhs.day << '/' << rhs.year;
+        //     return os;
+        // }
+    public:
+        Date(int m, int d, int y) : month(m), day(d), year(y) {}
+    private:
+        int month, day, year;
+    };
+
+    friend ostream& operator<<(ostream& os, const Date& rhs);
+public:
+
+    //Person(const string& name) : name(name) { }
+    //Person(const string& name) : name(name), bday() { }
+    Person(const string& name, int m, int d, int y)
+        : name(name), bday(m, d, y) { }
+private:    
+    string name;
+    Date bday;
+};
+
+int main() {
+    //Person john("John");
+    Person john("John", 2, 5, 2024);
+    cout << john << endl;
+
+
+    //Date someDate(1,1,1970);
+    //    Person::Date someDate(1,1,1970);
+}
+
+ostream& operator<<(ostream& os, const Person& rhs) {
+    //os << "Person: " << rhs.name; //  << ", dob: " << rhs.bday;
+    os << "Person: " << rhs.name << ", dob: " << rhs.bday;
+    return os;
+}
+
+//ostream& operator<<(ostream& os, const Date& rhs) {
+ostream& operator<<(ostream& os, const Person::Date& rhs) {
+    os << rhs.month << '/' << rhs.day << '/' << rhs.year;
+    return os;
+}
+
