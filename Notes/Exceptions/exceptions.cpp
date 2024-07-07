96282645028793e5cde7e17c75c58ae923dffdfe
JuJams <164974545+JuJams@users.noreply.github.com>
2024-07-07 14:20:48 -0400

    exceptions.cpp

---
+++ Notes/Exceptions/exceptions.cpp
@@ -0,0 +1,36 @@
+/*
+  CS2124
+  Exceptions and Assertions
+*/
+
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+
+#include <exception>
+#include <stdexcept>
+#include <cassert>
+
+using namespace std;
+
+
+void bar(int n) {
+    if (n > 200) {
+        // What to do?
+	//throw n;
+	throw out_of_range("bar");
+    }
+}
+void foo(int n) { bar(n); }
+
+int main() {
+    try {
+	foo(300);
+    }
+    // catch(out_of_range ex) {
+    // 	cout << "Caught out_of_range exception: " << ex.what() << endl;;
+    // }
+    catch(const exception& ex) {
+	cout << "Caught an exception: " << ex.what() << endl;;
+    }
+}
