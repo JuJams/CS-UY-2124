02e3f014e535329a3207c97d56fd54744f1b1afc
JuJams <164974545+JuJams@users.noreply.github.com>
2024-07-07 14:15:24 -0400

    FrogPrince.cpp

---
+++ Notes/Separate Compilation/FrogPrince.cpp
@@ -0,0 +1,27 @@
+// FrogPrince.cpp
+#include "FrogPrince.h"
+#include "Princess.h"
+#include <string>
+#include <iostream>
+using namespace std;
+
+namespace Fantasy {
+
+    //
+    // FrogPrince implementation code
+    //
+    FrogPrince::FrogPrince(const string& name) : name(name), spouse(nullptr) {}
+
+    const string& FrogPrince::getName() const { return name; }
+
+    ostream& operator<<(ostream& os, const FrogPrince& rhs) {
+        os << "FrogPrince: " << rhs.name;
+        if (rhs.spouse != nullptr) {
+            os << ", spouse: " << rhs.spouse->getName();
+        }
+        return os;
+    }
+
+    void FrogPrince::setSpouse(Princess* betrothed) { spouse = betrothed; }
+
+}
